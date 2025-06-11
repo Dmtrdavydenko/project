4fec2ed60b75f21f5622749d3a40fe392a263a9f
User <User@DESKTOP-Q9J86VR>
2025-06-12 03:06:44 +0700

    tex

--- public/textile.js
+++ public/textile.js
@@ -1,2 +1,19 @@
 console.log("textile");
 console.log(document.location.href);
+
+
+
+
+
+
+
+    const width = document.createElement("input");
+    width.type = "number";
+    width.min = 0;
+    width.step = 1;
+    const density = document.createElement("input");
+    density.type = "number";
+    density.min = 0;
+    density.step = 1;
+main.append(width);
+main.append(density);
