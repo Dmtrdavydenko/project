ef38b7f5fd428d3ff809bb074b661bf8fcf7aa5d
User <User@DESKTOP-Q9J86VR>
2025-06-12 03:25:03 +0700

    tex

--- public/textile.js
+++ public/textile.js
@@ -7,13 +7,47 @@ console.log(document.location.href);
 
 
 
-    const width = document.createElement("input");
-    width.type = "number";
-    width.min = 0;
-    width.step = 1;
-    const density = document.createElement("input");
-    density.type = "number";
-    density.min = 0;
-    density.step = 1;
+
+const width = document.createElement("input");
+width.type = "number";
+width.min = 0;
+width.step = 1;
+const density = document.createElement("input");
+density.type = "number";
+density.min = 0;
+density.step = 1;
+
+
+
+const density = document.createElement("input");
+density.type = "number";
+density.min = 0;
+density.step = 1;
+
+const send = document.createElement("button");
+send.textContent = "Send"
+
+
 main.append(width);
 main.append(density);
+main.append(send);
+function Textile(InputWidth,inputDensity){
+    this.width = InputWidth.valueAsNumber;
+    this.density = inputDensity.valueAsNumber;
+}
+send.addEventListener("click", async function (e) {
+    fetch(document.location.href, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({
+        action: "insert",
+        table: {
+          name: "textile",
+        },
+        data: new Textile(width,density)
+      }),
+    })
+      .then((response) => response.json())
+})
--- server.js
+++ server.js
@@ -14,11 +14,11 @@ const PORT = process.env.PORT || 3000;
 
 //считываем из env railway
 const dbConfig = {
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT || 3306, // Укажите порт по умолчанию, если переменная не установлена
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT || 3306, // Укажите порт по умолчанию, если переменная не установлена
 };
 
 const mysql = require('mysql2/promise');
@@ -55,18 +55,18 @@ main();
 
 
 const mimeTypes = {
-  ".txt":"text/plain",
-  ".html": "text/html",
-  ".js": "text/javascript",
-  ".css": "text/css",
-  ".ico": "image/x-icon",
-  ".png": "image/png",
-  ".jpg": "image/jpeg",
-  ".gif": "image/gif",
-  ".svg": "image/svg+xml",
-  ".json": "application/json",
-  ".woff": "font/woff",
-  ".woff2": "font/woff2",
+    ".txt": "text/plain",
+    ".html": "text/html",
+    ".js": "text/javascript",
+    ".css": "text/css",
+    ".ico": "image/x-icon",
+    ".png": "image/png",
+    ".jpg": "image/jpeg",
+    ".gif": "image/gif",
+    ".svg": "image/svg+xml",
+    ".json": "application/json",
+    ".woff": "font/woff",
+    ".woff2": "font/woff2",
 };
 
 const server = http.createServer();
@@ -78,81 +78,85 @@ function generateSessionId() {
 }
 
 function setCookie(res) {
-  const cookieName = "sessionId";
-  const sessionId = generateSessionId();
-  const maxAge = 36000; // Время жизни куки в секундах
-  // Формируем строку куки
-  const cookie = `${cookieName}=${sessionId}; HttpOnly; Secure; SameSite=Strict; Max-Age=${maxAge}`;
-  res.setHeader("Set-Cookie", cookie);
+    const cookieName = "sessionId";
+    const sessionId = generateSessionId();
+    const maxAge = 36000; // Время жизни куки в секундах
+    // Формируем строку куки
+    const cookie = `${cookieName}=${sessionId}; HttpOnly; Secure; SameSite=Strict; Max-Age=${maxAge}`;
+    res.setHeader("Set-Cookie", cookie);
 }
 
 function deleteCookie(res) {
-  const cookieName = "sessionId";
-  const sessionId = ""
-  const maxAge = -1; // Время жизни куки в секундах
-  // Формируем строку куки
-  const cookie = `${cookieName}=${sessionId}; HttpOnly; Secure; SameSite=Strict; Max-Age=${maxAge}`;
-  res.setHeader("Set-Cookie", cookie);
+    const cookieName = "sessionId";
+    const sessionId = ""
+    const maxAge = -1; // Время жизни куки в секундах
+    // Формируем строку куки
+    const cookie = `${cookieName}=${sessionId}; HttpOnly; Secure; SameSite=Strict; Max-Age=${maxAge}`;
+    res.setHeader("Set-Cookie", cookie);
 }
 
 
 
 server.on("request", (req, res) => {
-  
-  const parsedUrl = url.parse(req.url, true); 
-  let pathname = parsedUrl.pathname;
-  
-  if (req.method === "GET") {
-    const parsedUrl = new URL(req.url, "https://worktime.glitch.me/");
-    let pathName = parsedUrl.pathname;
-    let ext = path.extname(pathName);
-    if (pathName !== "/" && pathName[pathName.length - 1] === "/") {
-      res.writeHead(302, { Location: pathName.slice(0, -1) });
-      res.end();
-      return;
-    }
 
-    if (pathName === "/") {
-      ext = ".html";
-      pathName = "/index.html";
-    } else if (!ext) {
-      ext = ".html";
-      pathName += ext;
+    const parsedUrl = url.parse(req.url, true);
+    let pathname = parsedUrl.pathname;
+
+    if (req.method === "GET") {
+        const parsedUrl = new URL(req.url, "https://worktime.glitch.me/");
+        let pathName = parsedUrl.pathname;
+        let ext = path.extname(pathName);
+        if (pathName !== "/" && pathName[pathName.length - 1] === "/") {
+            res.writeHead(302, { Location: pathName.slice(0, -1) });
+            res.end();
+            return;
+        }
+
+        if (pathName === "/") {
+            ext = ".html";
+            pathName = "/index.html";
+        } else if (!ext) {
+            ext = ".html";
+            pathName += ext;
+        }
+        let filePath = path.join(process.cwd(), "/public", pathName);
+        console.log(pathName);
+        fs.exists(filePath, function (exists, err) {
+            if (!exists || !mimeTypes[ext]) {
+                console.log("File does not exist: " + pathName);
+                res.writeHead(404, { "Content-Type": "text/plain" });
+                res.write("404 Not Found");
+                res.end();
+                return;
+            }
+            res.writeHead(200, { "Content-Type": mimeTypes[ext] });
+            console.log(filePath);
+            const fileStream = fs.createReadStream(filePath);
+            fileStream.pipe(res);
+        });
     }
-    let filePath = path.join(process.cwd(), "/public", pathName);
-    console.log(pathName);
-    fs.exists(filePath, function (exists, err) {
-      if (!exists || !mimeTypes[ext]) {
-        console.log("File does not exist: " + pathName);
-        res.writeHead(404, { "Content-Type": "text/plain" });
-        res.write("404 Not Found");
-        res.end();
-        return;
-      }
-      res.writeHead(200, { "Content-Type": mimeTypes[ext] });
-      console.log(filePath);
-      const fileStream = fs.createReadStream(filePath);
-      fileStream.pipe(res);
-    });
-  }
-  if (req.method === "POST") {
-    let body = [];
-    // req.on("data", (chunk) => {
-    //     body.push(chunk);
-    //   }).on("end", () => {
-    //     body = Buffer.concat(body)
-    //     if (body != "") {
-    //       body = JSON.parse(body);
-    //       functionDB[body.action](body)
-    //         .then((resolve) => JSON.stringify(resolve))
-    //         .then((resolve) => res.end(resolve))
-    //         .catch(error=>{
-    //         res.end(error)
-    //         console.log(error);
-    //       })
-    //     }
-    // });
-  }
+    if (req.method === "POST") {
+        let body = [];
+        req.on("data", (chunk) => {
+            body.push(chunk);
+        }).on("end", () => {
+            body = Buffer.concat(body)
+            if (body != "") {
+                body = JSON.parse(body);
+                console.log(body);
+                console.log(req.url);
+                console.log(pathName);
+
+                //functionDB[body.action](body)
+                //  .then((resolve) => JSON.stringify(resolve))
+                //  .then((resolve) => res.end(resolve))
+                //  .catch(error=>{
+                //  res.end(error)
+                //  console.log(error);
+                //})
+            }
+        });
+    }
 });
 server.listen(PORT);
 console.log("Server listening on " + PORT);
