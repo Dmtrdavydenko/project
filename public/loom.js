8dd17f96db554141817b15e026feb75c3aa0dfc3
User <User@DESKTOP-Q9J86VR>
2025-07-25 22:30:15 +0700

    loom btn

--- public/loom.js
+++ public/loom.js
@@ -177,6 +177,21 @@ function createButtonsInBlockFromArray(field,containerId, numbersArray, reverse 
 }
 function toggle() {
     this.classList.toggle('active');
+    const localDateTime = getLocalDateTimeForMySQL();
+    console.log(localDateTime);
+}
+function getLocalDateTimeForMySQL() {
+    const now = new Date();
+
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0'); // Месяцы с 0
+    const day = String(now.getDate()).padStart(2, '0');
+
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
 
 /**
