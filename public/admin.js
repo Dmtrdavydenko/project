41eebdce8a8c9b86292469224c66d681db2e355b
User <User@DESKTOP-Q9J86VR>
2025-07-28 04:29:48 +0700

    textile join

--- public/admin.js
+++ public/admin.js
@@ -370,7 +370,7 @@ async function sqlQuery(sqlQueryString) {
             throw new Error(`Ошибка: ${response.status} ${response.statusText}`);
         }
 
-        const result = await response.text(); // Получаем JSON-ответ
+        const result = await response.json(); // Получаем JSON-ответ
         console.log(result); // Выводим результат в консоль
         //textAsk.value = response;
         textAsk.value = JSON.stringify(result);
