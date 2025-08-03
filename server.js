ff0d4fda7ecdf31515afbd8897008c3354681b68
User <User@DESKTOP-Q9J86VR>
2025-08-04 05:20:32 +0700

    textile join

--- server.js
+++ server.js
@@ -229,7 +229,8 @@ async function select(body) {
                     "FROM textile t " +
                     "JOIN circular_width width ON t.width_id = width.id " +
                     "JOIN density d ON t.density_id = d.id " +
-                    "WHERE width.circular_width = 50";
+                    "WHERE width.circular_width = 50 " +
+                    "ORDER BY d.density ASC;";
 
                 [descRows] = await connection.execute(`DESCRIBE \`${body.table.name}\``);
                 select.pri = descRows.find(row => row.Key === 'PRI')?.Field || null;
