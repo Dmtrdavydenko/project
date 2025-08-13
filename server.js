601261a3432aac15a1d3185c9ff9b085f2ae6689
User <User@DESKTOP-Q9J86VR>
2025-08-13 20:30:35 +0700

    textile join

--- server.js
+++ server.js
@@ -238,8 +238,11 @@ async function select(body) {
                     //"JOIN density                 d ON t.density_id = d.sleeve_density_id " +
                     "JOIN sleeve_width_density  swd ON t.wd_id = swd.sleeve_width_density_id " +
                     "JOIN sleeve_width           sw ON swd.sleeve_width_id = sw.sleeve_width_id " +
-                    "JOIN sleeve_density                 d ON swd.sleeve_density_id = d.sleeve_density_id "
+                    "JOIN sleeve_density                 d ON swd.sleeve_density_id = d.sleeve_density_id " +
+                    "JOIN warp_quantity                 wq ON t.warp_quantity = wq.warp_quantity "
 
+
+                
 
 
 
