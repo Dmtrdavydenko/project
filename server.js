efaeeaa0f2ec01daaa71edc884b2e1709f2a5483
User <User@DESKTOP-Q9J86VR>
2025-08-30 00:57:16 +0700

    textile join

--- server.js
+++ server.js
@@ -132,6 +132,7 @@ function transformKeys(inputObj) {
         'sleeve_width_density': 'sleeve_w_d_id',
         'yarn_type': 'yarn_id',
         'warp_quantity': 'quantity',
+        'weft_quantity': 'quantity',
         'Thread_Parameters': 'thread_densiti_id',
         'color': 'color_id',
         'additive': 'additive_id'
