e9f55deb08906b82374967026e0ab0dfeb6f72b1
User <User@DESKTOP-Q9J86VR>
2025-09-09 10:21:52 +0700

    thead manual edit

--- public/textile.js
+++ public/textile.js
@@ -441,7 +441,10 @@ async function queryTarget(event) {
     console.log('Кликнутая td:', td.textContent.trim());
     console.log('Индекс столбца:', tdIndex);
     console.log('Соответствующий thead (заголовок):', correspondingHeader);
-    console.log(rowData[correspondingHeader]);
+    console.log({ target: correspondingHeader, value: rowData[correspondingHeader]);
+
+
+
 
     //td.addEventListener('blur', async () => {
     //    td.contentEditable = "false";
