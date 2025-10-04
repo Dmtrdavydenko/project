f3914af61c657b469306214353f401bce3d98048
User <User@DESKTOP-Q9J86VR>
2025-10-04 13:55:28 +0700

    parent table cell edit

--- server.js
+++ server.js
@@ -294,9 +294,9 @@ async function select(body) {
             case "looms":
                 //const field = ["thread_id", "thread_density", "thread_length"];
                 sql = `SELECT 
-                    loom_number,
-                    type_id,
-                    FROM looms`;
+                    loom_number, 
+                    type_id 
+                    FROM looms;`;
                 //sql = "SELECT l.loom_id, l.loom_number, l.loom_name_str, l.loom_nameId, s.speed AS loom_speed, l.weft FROM looms l JOIN speed s ON l.loom_speed = s.speed_id";
                 break;
             case "Thread_Parameters":
