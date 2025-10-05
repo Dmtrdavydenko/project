f4e9162d6342a22cf3ddc7a62c00d9d9ad5a0bfd
User <User@DESKTOP-Q9J86VR>
2025-10-05 10:39:35 +0700

    parent table cell edit

--- public/js/loom.js
+++ public/js/loom.js
@@ -124,7 +124,7 @@ async function loadAndRenderButtons(field = "loom_number") {
                 //    "JOIN sleeve_width_density swd ON t.wd_id = swd.sleeve_width_density_id " +
                 //    "JOIN sleeve_width sw ON swd.sleeve_width_id = sw.sleeve_width_id " +
                 //    "JOIN sleeve_density d ON swd.sleeve_density_id = d.sleeve_density_id;",
-                query: "SELECT CONCAT(sw.sleeve_width, '/', sd.density) AS sleeve_width_density " + field + " " +
+                query: "SELECT CONCAT(sw.sleeve_width, '/', sd.density) AS sleeve_width_density, " + field + " " +
                     "FROM looms " +
                     "LEFT JOIN sleeve_width_density swd ON looms.type_id = swd.sleeve_width_density_id " +
                     "LEFT JOIN sleeve_width sw ON swd.sleeve_width_id = sw.sleeve_width_id " +
