c04cb35f2e6b434fbfcbec13efc50a6edf8edcd9
User <User@DESKTOP-Q9J86VR>
2025-10-16 16:46:18 +0700

    select f1

--- server.js
+++ server.js
@@ -341,23 +341,24 @@ async function select(body) {
                         -- l.loom_nameId,
                         -- s.speed AS loom_speed,
                         -- l.weft,
-                        type.yarn_name,
-                        sw.sleeve_width as s_width,
-                        d.density as s_density,
-                        CASE
-                            WHEN type.yarn_name = "warp" THEN warp.warp_quantity
-                            WHEN type.yarn_name = "weft" THEN weft.weft_quantity
-                            ELSE NULL
-                            END as quantity,
-                        thread.thread_density as t_density,
-                        c.color,
-                        thread.thread_length as t_length,
-                        ad.additive_name,
-                        m.*
+                        -- type.yarn_name,
+                        -- sw.sleeve_width as s_width,
+                        -- d.density as s_density,
+                        -- CASE
+                            -- WHEN type.yarn_name = "warp" THEN warp.warp_quantity
+                            -- WHEN type.yarn_name = "weft" THEN weft.weft_quantity
+                            -- ELSE NULL
+                            -- END as quantity,
+                        -- thread.thread_density as t_density,
+                        -- c.color,
+                        -- thread.thread_length as t_length,
+                        -- ad.additive_name,
+                        -- m.*
                      FROM looms l 
                      JOIN speed s ON l.loom_speed = s.speed_id 
                      LEFT JOIN \`manual\` m ON l.type_id = m.sleeve_w_d_id
-                     LEFT JOIN sleeve_width_density swd                    ON m.sleeve_w_d_id = swd.sleeve_width_density_id
+                    -- LEFT JOIN sleeve_width_density swd                    ON m.sleeve_w_d_id = swd.sleeve_width_density_id
+                     LEFT JOIN sleeve_width_density swd                    ON l.type_id = swd.sleeve_width_density_id
                      LEFT JOIN sleeve_width sw                    ON swd.sleeve_width_id = sw.sleeve_width_id
                      LEFT JOIN sleeve_density d                    ON swd.sleeve_density_id = d.sleeve_density_id
                      LEFT JOIN Thread_Parameters thread ON m.thread_densiti_id = thread.thread_id
@@ -372,6 +373,10 @@ async function select(body) {
                 const field = ["thread_id", "thread_density", "thread_length"];
                 //sql = "SELECT t." + field.join(", t.") + ", c.color FROM threadPP t JOIN color c ON t.color_id = c.color_id";
                 sql = "SELECT * FROM Thread_Parameters";
+
+
+
+                
 
 
                 break;
