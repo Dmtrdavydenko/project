7df3bfb6e99b56e1915b60895781242fff8a2c0c
User <User@DESKTOP-Q9J86VR>
2025-11-03 02:14:02 +0700

    looms join select params

--- server.js
+++ server.js
@@ -492,69 +492,69 @@ WHERE type.yarn_name = 'warp' AND thread.thread_density = 105 AND ad.additive_na
 
 
                      `;
-                sql = `WITH aggregated AS (
-    SELECT 
-        l.loom_id,
-        l.loom_number,
-        machine.machine_name,
-        sw.sleeve_width as width,
-        d.density,
-        type.yarn_name,
-        CASE
-            WHEN type.yarn_name = "warp" THEN SUM(warp.warp_quantity)
-            WHEN type.yarn_name = "weft" THEN SUM(weft.weft_quantity)
-            ELSE NULL
-        END as quantity,
-        CASE
-            WHEN type.yarn_name = "weft" AND SUM(weft.weft_quantity) > 25 THEN CEIL(SUM(weft.weft_quantity) * 0.1 * MAX(sw.sleeve_width) * 2 * MAX(s.speed) * 720 / (SUM(weft.weft_quantity) * 10) * 0.89)
-            WHEN type.yarn_name = "weft" AND SUM(weft.weft_quantity) < 25 THEN CEIL(SUM(weft.weft_quantity) * 0.1 * MAX(sw.sleeve_width) * 2 * MAX(s.speed) * 720 / (SUM(weft.weft_quantity) * 20) * 0.89)
-            ELSE NULL
-        END as weft_lenth,
-        CASE
-            WHEN type.yarn_name = "weft" AND SUM(weft.weft_quantity) > 25 THEN CEIL(MAX(s.speed) * 720 / (SUM(weft.weft_quantity) * 10) * 0.89)
-            WHEN type.yarn_name = "weft" AND SUM(weft.weft_quantity) < 25 THEN CEIL(MAX(s.speed) * 720 / (SUM(weft.weft_quantity) * 20) * 0.89)
-            ELSE NULL
-        END as productivity,
-        CASE
-            WHEN type.yarn_name = "weft" AND SUM(weft.weft_quantity) > 25 THEN CEIL(SUM(weft.weft_quantity) * 0.1 * MAX(sw.sleeve_width) * 2 * MAX(s.speed) * 720 / (SUM(weft.weft_quantity) * 10) * 0.89 / MAX(thread.thread_length))
-            WHEN type.yarn_name = "weft" AND SUM(weft.weft_quantity) < 25 THEN CEIL(SUM(weft.weft_quantity) * 0.1 * MAX(sw.sleeve_width) * 2 * MAX(s.speed) * 720 / (SUM(weft.weft_quantity) * 20) * 0.89 / MAX(thread.thread_length))
-            ELSE NULL
-        END as quantity_weft,
-        MAX(thread.thread_density) as thread_density,  -- Тип нити (90, 78, 105 и т.д.)
-        MAX(c.color) as color,
-        MAX(ad.additive_name) as additive_name
-    FROM looms l
-    JOIN machine ON l.machine_id = machine.machine_id
-    LEFT JOIN speed s ON machine.speed_id = s.speed_id
-    LEFT JOIN sleeve_width_density swd ON l.type_id = swd.sleeve_width_density_id
-    LEFT JOIN sleeve_width sw ON swd.sleeve_width_id = sw.sleeve_width_id
-    LEFT JOIN sleeve_density d ON swd.sleeve_density_id = d.sleeve_density_id
-    LEFT JOIN \`manual\` m ON l.type_id = m.sleeve_w_d_id AND l.modifier_id = m.additive_id
-    LEFT JOIN Thread_Parameters thread ON m.thread_densiti_id = thread.thread_id
-    LEFT JOIN color c ON m.color_id = c.color_id
-    LEFT JOIN additive ad ON m.additive_id = ad.id
-    LEFT JOIN yarn_type type ON m.yarn_id = type.yarn_id
-    LEFT JOIN warp_quantity warp ON m.quantity_id = warp.warp_id
-    LEFT JOIN weft_quantity weft ON m.quantity_id = weft.weft_id
-    GROUP BY l.loom_id, type.yarn_name
-)
-SELECT 
-    yarn_name,
-    thread_density,  -- Теперь группируем по этому полю тоже
-    SUM(quantity) as total_quantity,
-    SUM(weft_lenth) as total_weft_lenth,
-    SUM(productivity) as total_productivity,
-    SUM(quantity_weft) as total_quantity_weft,
-    GROUP_CONCAT(DISTINCT machine_name ORDER BY machine_name) as machines,
-    GROUP_CONCAT(DISTINCT width ORDER BY width) as widths,
-    GROUP_CONCAT(DISTINCT density ORDER BY density) as densities,
-    GROUP_CONCAT(DISTINCT color ORDER BY color) as colors,
-    GROUP_CONCAT(DISTINCT additive_name ORDER BY additive_name) as additives
-FROM aggregated
-GROUP BY yarn_name, thread_density  -- Изменено: группировка по yarn_name + thread_density
-ORDER BY yarn_name, thread_density;
+//                sql = `WITH aggregated AS (
+//    SELECT 
+//        l.loom_id,
+//        l.loom_number,
+//        machine.machine_name,
+//        sw.sleeve_width as width,
+//        d.density,
+//        type.yarn_name,
+//        CASE
+//            WHEN type.yarn_name = "warp" THEN SUM(warp.warp_quantity)
+//            WHEN type.yarn_name = "weft" THEN SUM(weft.weft_quantity)
+//            ELSE NULL
+//        END as quantity,
+//        CASE
+//            WHEN type.yarn_name = "weft" AND SUM(weft.weft_quantity) > 25 THEN CEIL(SUM(weft.weft_quantity) * 0.1 * MAX(sw.sleeve_width) * 2 * MAX(s.speed) * 720 / (SUM(weft.weft_quantity) * 10) * 0.89)
+//            WHEN type.yarn_name = "weft" AND SUM(weft.weft_quantity) < 25 THEN CEIL(SUM(weft.weft_quantity) * 0.1 * MAX(sw.sleeve_width) * 2 * MAX(s.speed) * 720 / (SUM(weft.weft_quantity) * 20) * 0.89)
+//            ELSE NULL
+//        END as weft_lenth,
+//        CASE
+//            WHEN type.yarn_name = "weft" AND SUM(weft.weft_quantity) > 25 THEN CEIL(MAX(s.speed) * 720 / (SUM(weft.weft_quantity) * 10) * 0.89)
+//            WHEN type.yarn_name = "weft" AND SUM(weft.weft_quantity) < 25 THEN CEIL(MAX(s.speed) * 720 / (SUM(weft.weft_quantity) * 20) * 0.89)
+//            ELSE NULL
+//        END as productivity,
+//        CASE
+//            WHEN type.yarn_name = "weft" AND SUM(weft.weft_quantity) > 25 THEN CEIL(SUM(weft.weft_quantity) * 0.1 * MAX(sw.sleeve_width) * 2 * MAX(s.speed) * 720 / (SUM(weft.weft_quantity) * 10) * 0.89 / MAX(thread.thread_length))
+//            WHEN type.yarn_name = "weft" AND SUM(weft.weft_quantity) < 25 THEN CEIL(SUM(weft.weft_quantity) * 0.1 * MAX(sw.sleeve_width) * 2 * MAX(s.speed) * 720 / (SUM(weft.weft_quantity) * 20) * 0.89 / MAX(thread.thread_length))
+//            ELSE NULL
+//        END as quantity_weft,
+//        MAX(thread.thread_density) as thread_density,  -- Тип нити (90, 78, 105 и т.д.)
+//        MAX(c.color) as color,
+//        MAX(ad.additive_name) as additive_name
+//    FROM looms l
+//    JOIN machine ON l.machine_id = machine.machine_id
+//    LEFT JOIN speed s ON machine.speed_id = s.speed_id
+//    LEFT JOIN sleeve_width_density swd ON l.type_id = swd.sleeve_width_density_id
+//    LEFT JOIN sleeve_width sw ON swd.sleeve_width_id = sw.sleeve_width_id
+//    LEFT JOIN sleeve_density d ON swd.sleeve_density_id = d.sleeve_density_id
+//    LEFT JOIN \`manual\` m ON l.type_id = m.sleeve_w_d_id AND l.modifier_id = m.additive_id
+//    LEFT JOIN Thread_Parameters thread ON m.thread_densiti_id = thread.thread_id
+//    LEFT JOIN color c ON m.color_id = c.color_id
+//    LEFT JOIN additive ad ON m.additive_id = ad.id
+//    LEFT JOIN yarn_type type ON m.yarn_id = type.yarn_id
+//    LEFT JOIN warp_quantity warp ON m.quantity_id = warp.warp_id
+//    LEFT JOIN weft_quantity weft ON m.quantity_id = weft.weft_id
+//    GROUP BY l.loom_id, type.yarn_name
+//)
+//SELECT 
+//    yarn_name,
+//    thread_density,  -- Теперь группируем по этому полю тоже
+//    SUM(quantity) as total_quantity,
+//    SUM(weft_lenth) as total_weft_lenth,
+//    SUM(productivity) as total_productivity,
+//    SUM(quantity_weft) as total_quantity_weft,
+//    GROUP_CONCAT(DISTINCT machine_name ORDER BY machine_name) as machines,
+//    GROUP_CONCAT(DISTINCT width ORDER BY width) as widths,
+//    GROUP_CONCAT(DISTINCT density ORDER BY density) as densities,
+//    GROUP_CONCAT(DISTINCT color ORDER BY color) as colors,
+//    GROUP_CONCAT(DISTINCT additive_name ORDER BY additive_name) as additives
+//FROM aggregated
+//GROUP BY yarn_name, thread_density  -- Изменено: группировка по yarn_name + thread_density
+//ORDER BY yarn_name, thread_density;
 
-`
+//`
 
 //                                          UNION ALL
 
