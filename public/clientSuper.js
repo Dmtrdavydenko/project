fffb765faf837a4c7574dde5adc211d7a19ff09c
User <User@DESKTOP-Q9J86VR>
2025-11-24 23:08:18 +0700

    check

--- public/clientSuper.js
+++ public/clientSuper.js
@@ -342,7 +342,7 @@ const Thread = new DataTape("https://worktime.up.railway.app/textile");
             time.type = "time";
             time.placeholder = "с";
             time.valueAsNumber = dataTime[i] || 0;
-            //td.append(time);
+            td.append(time);
             let select = dropListSelectTex(thread[0]);
             select.name = i;
             td.append(select);
