bb157feac60a388ff0c329ea4828aca4c13ca0d8
User <User@DESKTOP-Q9J86VR>
2025-11-26 01:50:28 +0700

    check

--- public/js/viewing.js
+++ public/js/viewing.js
@@ -131,28 +131,171 @@ class DataTape {
 
 
 
-const Tape = new DataTape("https://worktime.up.railway.app/textile");
-const Thread = new DataTape("https://worktime.up.railway.app/textile");
-const Time = new DataTape("https://worktime.up.railway.app/textile");
+//const Tape = new DataTape("https://worktime.up.railway.app/textile");
+//const Thread = new DataTape("https://worktime.up.railway.app/textile");
+//const Time = new DataTape("https://worktime.up.railway.app/textile");
 
 (async () => {
-    const tape = await Tape.loadData("getTape");
-    const thread = await Thread.loadData("getThreads");
-    const task = await Time.loadData("getTime");
+    //const tape = await Tape.loadData("getTape");
+    //const thread = await Thread.loadData("getThreads");
+    //const task = await Time.loadData("getTime");
+    const task = [[]];
 
-    console.log(Tape);
-    console.log(tape);
+    //console.log(Tape);
+    //console.log(tape);
 
-    console.log(Thread);
-    console.log(thread);
+    //console.log(Thread);
+    //console.log(thread);
 
-    console.log(Time);
-    console.log(task);
+    //console.log(Time);
+    //console.log(task);
 
-    console.log(task[0]);
+    //console.log(task[0]);
 
 
-    const schedule = task[0].map(item => ({ time: item.time_milliseconds, name: `${item.thread_density} ${item.color} ${item.additive_name}`  }));
+
+
+    task[0] = [
+        {
+            "id": 1,
+            "time_seconds": 1763150400,
+            "time_milliseconds": 1763150400000,
+            "thread_density": 90,
+            "color": "белая",
+            "additive_name": "светостабилизатор 1,5%"
+        },
+        {
+            "id": 2,
+            "time_seconds": 1763152380,
+            "time_milliseconds": 1763152380000,
+            "thread_density": 90,
+            "color": "белая",
+            "additive_name": "нет"
+        },
+        {
+            "id": 3,
+            "time_seconds": 1763154360,
+            "time_milliseconds": 1763154360000,
+            "thread_density": 90,
+            "color": "белая",
+            "additive_name": "нет"
+        },
+        {
+            "id": 4,
+            "time_seconds": 1763156340,
+            "time_milliseconds": 1763156340000,
+            "thread_density": 90,
+            "color": "белая",
+            "additive_name": "нет"
+        },
+        {
+            "id": 5,
+            "time_seconds": 1763158320,
+            "time_milliseconds": 1763158320000,
+            "thread_density": 90,
+            "color": "белая",
+            "additive_name": "нет"
+        },
+        {
+            "id": 6,
+            "time_seconds": 1763160300,
+            "time_milliseconds": 1763160300000,
+            "thread_density": 90,
+            "color": "белая",
+            "additive_name": "нет"
+        },
+        {
+            "id": 7,
+            "time_seconds": 1763162280,
+            "time_milliseconds": 1763162280000,
+            "thread_density": 90,
+            "color": "белая",
+            "additive_name": "нет"
+        },
+        {
+            "id": 8,
+            "time_seconds": 1763164260,
+            "time_milliseconds": 1763164260000,
+            "thread_density": 90,
+            "color": "белая",
+            "additive_name": "нет"
+        },
+        {
+            "id": 9,
+            "time_seconds": 1763169600,
+            "time_milliseconds": 1763169600000,
+            "thread_density": 90,
+            "color": "белая",
+            "additive_name": "нет"
+        },
+        {
+            "id": 10,
+            "time_seconds": 1763172000,
+            "time_milliseconds": 1763172000000,
+            "thread_density": 90,
+            "color": "белая",
+            "additive_name": "нет"
+        },
+        {
+            "id": 11,
+            "time_seconds": 1763174400,
+            "time_milliseconds": 1763174400000,
+            "thread_density": 90,
+            "color": "белая",
+            "additive_name": "нет"
+        },
+        {
+            "id": 12,
+            "time_seconds": 1763176800,
+            "time_milliseconds": 1763176800000,
+            "thread_density": 90,
+            "color": "белая",
+            "additive_name": "нет"
+        },
+        {
+            "id": 13,
+            "time_seconds": 1763179200,
+            "time_milliseconds": 1763179200000,
+            "thread_density": 90,
+            "color": "белая",
+            "additive_name": "нет"
+        },
+        {
+            "id": 14,
+            "time_seconds": 1763181600,
+            "time_milliseconds": 1763181600000,
+            "thread_density": 90,
+            "color": "белая",
+            "additive_name": "нет"
+        },
+        {
+            "id": 15,
+            "time_seconds": 1763184000,
+            "time_milliseconds": 1763184000000,
+            "thread_density": 90,
+            "color": "белая",
+            "additive_name": "нет"
+        },
+        {
+            "id": 16,
+            "time_seconds": 1763186400,
+            "time_milliseconds": 1763186400000,
+            "thread_density": 90,
+            "color": "белая",
+            "additive_name": "нет"
+        },
+        {
+            "id": 17,
+            "time_seconds": 1763188800,
+            "time_milliseconds": 1763188800000,
+            "thread_density": 90,
+            "color": "белая",
+            "additive_name": "нет"
+        }
+    ]
+
+
+    const schedule = task[0].map(item => ({ time: item.time_milliseconds, name: `${item.thread_density} ${item.color} ${item.additive_name}` }));
     let intervalSecondsJob = [0];
 
     for (let i = 1; i < task[0].length; i++) {
@@ -160,19 +303,68 @@ const Time = new DataTape("https://worktime.up.railway.app/textile");
 
     }
     let nameThread = [];
-    for (var i = 0; i < intervalSecondsJob.length; i++) {
-        const foundItem = tape[0].find(item => item.time_seconds === intervalSecondsJob[i]);
-        nameThread.push(foundItem ? foundItem.density + " уток" : "основа");
+    //for (var i = 0; i < intervalSecondsJob.length; i++) {
+    //    const foundItem = tape[0].find(item => item.time_seconds === intervalSecondsJob[i]);
+    //    nameThread.push(foundItem ? foundItem.density + " уток" : "основа");
+    //}
+
+    console.log(intervalSecondsJob);
+
+
+
+
+
+    function make() {
+        const timeList = document.createElement("ol");
+        const section = document.createElement("section");
+        const h2 = document.createElement("h2");
+        h2.textContent = "Экструдер 1";
+        section.appendChild(h2);
+        section.appendChild(timeList);
+
+        section.classList.add("class");
+        main.appendChild(section);
+
+        const now = new Date();
+
+        const startOfDay2 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours() + 7, now.getMinutes(), 0, 0);
+
+        const currentTime = startOfDay2.getTime();
+
+        // Заполнение списка с input type="time"
+        const stTime = getCurrentMinutes();
+
+        schedule.forEach((item) => {
+            const li = document.createElement('li');
+            const timeInput = new viewTime(item.time);
+            const threadName = new viewText(item.name);
+            const slotMinutes = item.time;
+            let sum = item.time;
+            if (sum < currentTime) {
+                timeInput.disabled = true;
+            } else {
+                timeInput.classList.add('active');
+            }
+
+            li.appendChild(timeInput);
+            li.appendChild(threadName);
+            li.classList.add("time");
+
+            timeList.appendChild(li);
+        });
 
 
     }
-    
-    console.log(intervalSecondsJob);
 
-    const timeList = document.createElement("ol");
+    make();
+    make();
+    make();
+
+
     //main.classList.add("container");
     //timeList.classList.add("time-list");
-    main.appendChild(timeList);
+
+    //main.appendChild(timeList);
     const infoDiv = 0;
 
     // Генерация расписания: от 00:00 до 23:20 с шагом 40 минут
@@ -203,31 +395,9 @@ const Time = new DataTape("https://worktime.up.railway.app/textile");
 
 
 
-    const now = new Date();
 
-    const startOfDay2 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours() + 7, now.getMinutes(), 0, 0);
-
-    const currentTime = startOfDay2.getTime();
-
-    // Заполнение списка с input type="time"
-    const stTime = getCurrentMinutes();
-    schedule.forEach((item) => {
-        const li = document.createElement('li');
-        const timeInput = new viewTime(item.time);
-        const threadName = new viewText(item.name);
-        const slotMinutes = item.time;
-        let sum = item.time;
-        if (sum < currentTime) {
-            timeInput.disabled = true;
-        } else {
-            timeInput.classList.add('active');
-        }
-
-        li.appendChild(timeInput);
-        li.appendChild(threadName);
-        timeList.appendChild(li);
-    });
-}) ();
+    main.classList.add("container");
+})();
 
 
 
