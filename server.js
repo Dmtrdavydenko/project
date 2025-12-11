0dd6113cd3bf3dcccdb21b04541c2ac718aa1221
User <User@DESKTOP-Q9J86VR>
2025-12-12 06:11:15 +0700

    check

--- server.js
+++ server.js
@@ -852,7 +852,7 @@ WHERE type.yarn_name = 'warp' AND thread.thread_density = 105 AND ad.additive_na
             Field: select.fields,
             F: select.sqlFields,
             key: select.pri,
-            get1: await getMeta(body),
+            get4: await getMeta(body),
             get2: await getMeta2(body),
             get3: await getMeta3(body)
         };
