7129c853518975c4dc62e92d39942943da8b854c
User <User@DESKTOP-Q9J86VR>
2025-12-12 06:31:12 +0700

    check

--- server.js
+++ server.js
@@ -324,10 +324,10 @@ async function getMeta1(body) {
             EXTRA,
             COLUMN_DEFAULT
             FROM INFORMATION_SCHEMA.COLUMNS
-            WHERE TABLE_NAME = ?
+            WHERE TABLE_NAME = "Tape"
             AND TABLE_SCHEMA = "railway"`;
 
-            return await connection.execute(sql, body.table.name);
+            return await connection.execute(sql);
         } catch (err) {
             console.error('Ошибка:', err);
             throw err;
@@ -847,8 +847,8 @@ WHERE type.yarn_name = 'warp' AND thread.thread_density = 105 AND ad.additive_na
         //select.name = body.table.name;
         console.log("Клиент " + sql);
         return {
-            all,
-            rows,
+            //all,
+            //rows,
             //Field: select.fields,
             //F: select.sqlFields,
             //key: select.pri,
