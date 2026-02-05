7a673d1771ccb0162514c47396126390cab17a0a
User <User@DESKTOP-Q9J86VR>
2026-02-05 14:26:19 +0700

    hh cont

--- public/models/app.js
+++ public/models/app.js
@@ -228,7 +228,7 @@ class WordClassificationApp {
         const english = "abcdefghijklmnopqrstuvwxyz";
         const number = "0123456789+-'";
         const alphabet = russian + english + number;
-        const embeddingDim = 64;
+        const embeddingDim = 1024;
 
         // Инициализируем только один раз
         if (!this.globalCharEmbeddings) {
@@ -247,7 +247,7 @@ class WordClassificationApp {
         //this.Embeddings = embeddingDim;
         //console.log(embeddingDim);
         const vector = Array(embeddingDim).fill(0);
-        const normalizedWord = word.toLowerCase().replace(/[^a-zа-яё0-9+-']/g, '');
+        const normalizedWord = word.toLowerCase().replace(/[^a-zа-яё0-9+'-]/g, '');
 
         if (normalizedWord.length === 0) return vector;
 
