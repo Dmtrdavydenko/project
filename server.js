b05b2b85ca7c5135e1a72f131e3b046ffa590f14
User <User@DESKTOP-Q9J86VR>
2026-04-27 05:56:32 +0700

    check edit

--- server.js
+++ server.js
@@ -647,7 +647,7 @@ WHERE type.yarn_name = 'warp' AND thread.thread_density = 105 AND ad.additive_na
                         -- l.loom_id,
                         l.loom_number,
                         -- machine.machine_name,
-                        -- s.speed,
+                        loom_machine.name,
                         -- machine.shuttle,
                         sw.sleeve_width as fabric_width,
                         d.sleeve_density as fabric_density,
@@ -659,14 +659,14 @@ WHERE type.yarn_name = 'warp' AND thread.thread_density = 105 AND ad.additive_na
                             ELSE NULL
                         END as quantity,
                         CASE
-                            WHEN type.yarn_id = 2 AND weft.weft_quantity > 25 THEN CEIL(weft.weft_quantity * 0.1 * sw.sleeve_width * 2 * s.speed * 720 / (weft.weft_quantity * 10) * 0.89)
-                            WHEN type.yarn_id = 2 AND weft.weft_quantity < 25 THEN CEIL(weft.weft_quantity * 0.1 * sw.sleeve_width * 2 * s.speed * 720 / (weft.weft_quantity * 20) * 0.89)
+                            WHEN type.yarn_id = 2 AND weft.weft_quantity > 25 THEN CEIL(weft.weft_quantity * 0.1 * sw.sleeve_width * 2 * loom_machine.ppm * 720 / (weft.weft_quantity * 10) * 0.89)
+                            WHEN type.yarn_id = 2 AND weft.weft_quantity < 25 THEN CEIL(weft.weft_quantity * 0.1 * sw.sleeve_width * 2 * loom_machine.ppm * 720 / (weft.weft_quantity * 20) * 0.89)
                             ELSE NULL
                         END as weft_lenth,
 
                         CASE
-                            WHEN type.yarn_id = 2 AND weft.weft_quantity > 25 THEN CEIL(s.speed * 720 / (weft.weft_quantity * 10) * 0.89)
-                            WHEN type.yarn_id = 2 AND weft.weft_quantity < 25 THEN CEIL(s.speed * 720 / (weft.weft_quantity * 20) * 0.89)
+                            WHEN type.yarn_id = 2 AND weft.weft_quantity > 25 THEN CEIL(loom_machine.ppm * 720 / (weft.weft_quantity * 10) * 0.89)
+                            WHEN type.yarn_id = 2 AND weft.weft_quantity < 25 THEN CEIL(loom_machine.ppm * 720 / (weft.weft_quantity * 20) * 0.89)
                             ELSE NULL
                         END as productivity,
 
@@ -674,8 +674,8 @@ WHERE type.yarn_name = 'warp' AND thread.thread_density = 105 AND ad.additive_na
 
 
                         CASE
-                            WHEN type.yarn_id = 2 AND weft.weft_quantity > 25 THEN CEIL(weft.weft_quantity * 0.1 * sw.sleeve_width * 2 * s.speed * 720 / (weft.weft_quantity * 10) * 0.89 / Tape.length)
-                            WHEN type.yarn_id = 2 AND weft.weft_quantity < 25 THEN CEIL(weft.weft_quantity * 0.1 * sw.sleeve_width * 2 * s.speed * 720 / (weft.weft_quantity * 20) * 0.89 / Tape.length)
+                            WHEN type.yarn_id = 2 AND weft.weft_quantity > 25 THEN CEIL(weft.weft_quantity * 0.1 * sw.sleeve_width * 2 * loom_machine.ppm * 720 / (weft.weft_quantity * 10) * 0.89 / Tape.length)
+                            WHEN type.yarn_id = 2 AND weft.weft_quantity < 25 THEN CEIL(weft.weft_quantity * 0.1 * sw.sleeve_width * 2 * loom_machine.ppm * 720 / (weft.weft_quantity * 20) * 0.89 / Tape.length)
                             ELSE NULL
                         END as quantity_weft,
 
@@ -688,7 +688,7 @@ WHERE type.yarn_name = 'warp' AND thread.thread_density = 105 AND ad.additive_na
                      FROM looms l
 
                      JOIN loom_machine ON l.machine_id = loom_machine.id
-                     LEFT JOIN speed s ON machine.speed_id = s.speed_id
+                     -- LEFT JOIN speed s ON machine.speed_id = s.speed_id
 
 
                      LEFT JOIN sleeve_width_density swd ON l.type_id = swd.sleeve_width_density_id
