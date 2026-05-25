7d5261ada829c3a4bef5256826e14f8e91a03564
Dmitry <dima.davydenko.c@mail.ru>
2026-05-26 03:37:24 +0700

    new table endpoint

--- src/sql/endpoint/insert.sql
+++ src/sql/endpoint/insert.sql
@@ -4,12 +4,8 @@ INSERT INTO user_endpoints (
     user_agent,
     language
 )
-
 VALUES (?, ?, ?, ?)
-
 ON DUPLICATE KEY UPDATE
-
-    ip = VALUES(ip),
     user_agent = VALUES(user_agent),
     language = VALUES(language),
     last_visit = CURRENT_TIMESTAMP;
