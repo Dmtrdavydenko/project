5578cf5ce5e4683d9e39470d16494330b43600d5
Dmitry <dima.davydenko.c@mail.ru>
2026-05-31 11:12:20 +0700

    test server

---
+++ public/js/profile.js
@@ -0,0 +1,77 @@
+(async () => {
+    let userProfileLogin = profile.textContent.trim();
+    const dataProfile = {};
+    dataProfile.login = "Davydenko";
+    profile.textContent = userProfileLogin.replace(/X/g, dataProfile.login);
+    id.value = "1";
+    fio.value = "Davydenko Dmitry O.";
+    birthDate.value = "2001-07-06";
+})();
+form.addEventListener("submit", async function (event) {
+
+    event.preventDefault();
+    const data = user;
+
+    try {
+
+        const response = await fetch(api, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data),
+        });
+
+        const result = await response.json();
+        console.log(result);
+
+        //result.message || "Успешно";
+        btn.innerText = 0 || "Успешно";
+
+    }
+    catch (error) {
+        btn.innerText = "Ошибка соединения с сервером";
+        console.error(error);
+    }
+
+});
+//(async (api) => {
+//    async function updateUserProfile(api) {
+
+//        const response = await fetch(api);
+//        const data = await response.json();
+//        let userProfileLogin = profile.textContent.trim();
+//        const dataProfile = {};
+//        dataProfile.login = "Davydenko";
+//        profile.textContent = userProfileLogin.replace(/X/g, dataProfile.login);
+//        id.value = "1";
+
+
+//        const user = new Object(null);
+//        user.fio = fio.value;
+//        user.birthDate = birthDate.value;
+//        return user
+//    }
+//    console.log(await updateUserProfile(api));
+//})("/api/profile/insert");
+
+
+//(async (api) => {
+
+//    const response = await fetch(api);
+//    const data = await response.json();
+//    let userProfileLogin = profile.textContent.trim();
+//    const dataProfile = {};
+//    dataProfile.login = "Davydenko";
+//    profile.textContent = userProfileLogin.replace(/X/g, dataProfile.login);
+//    id.value = "1";
+
+
+//    const user = new Object(null);
+//    user.fio = fio.value;
+//    user.birthDate = birthDate.value;
+//    return user
+//})("/api/profile/insert")
+//    .then(user => {
+//        console.log(user);
+//    })
