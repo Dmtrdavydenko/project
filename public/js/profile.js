12f599b444b461dc51718ed83294b59ce5f05ef1
Dmitry <dima.davydenko.c@mail.ru>
2026-06-01 11:35:59 +0700

    test server

--- public/js/profile.js
+++ public/js/profile.js
@@ -104,7 +104,10 @@ quit.addEventListener("submit", async function (event) {
     fio.value = data.profile.fio;
     birthDate.value = data.profile.birth_date?.split("T")[0] ?? "";
 
-
+    const test = data.permissions.map(i => `<div class="label">${i.permission_name}</div><div>${i.description}</div>`).join("");
+    console.log(test);
+    permission.innerHTML = test;
+    console.log(data);
 
     return data;
 })("/api/profile")
