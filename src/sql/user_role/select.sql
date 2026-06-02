00e23017cb8ec43dd038fd229779d7a0fd41ec66
Dmitry <dima.davydenko.c@mail.ru>
2026-06-02 11:37:34 +0700

    add role user in profile

--- src/sql/user_role/select.sql
+++ src/sql/user_role/select.sql
@@ -1,9 +1,9 @@
 SELECT 
-  user_id,
-  role_id,
+  ur.user_id,
+  ur.role_id,
   role_name,
   description,
   login
 FROM user_role ur
 JOIN roles r ON r.role_id = ur.role_id
-JOIN users u ON u.user_id = ur.user_id;
+JOIN users u ON u.user_id = ur.user_id
