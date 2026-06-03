700bc7a3380a462cba00231a31f988638d352337
Dmitry <dima.davydenko.c@mail.ru>
2026-06-03 12:21:31 +0700

    add role user in profile

---
+++ src/sql/user_role/select_by_user_id.sql
@@ -0,0 +1,10 @@
+SELECT 
+  ur.user_id,
+  ur.role_id,
+  role_name,
+  description,
+  login
+FROM user_role ur
+JOIN roles r ON r.role_id = ur.role_id
+JOIN users u ON u.user_id = ur.user_id
+WHERE ur.user_id = ?
