963908364e195d910dab527f7f5d0e1dbec54941
Dmitry <dima.davydenko.c@mail.ru>
2026-06-29 06:33:15 +0700

    check

--- src/sql/tape_length/select.sql
+++ src/sql/tape_length/select.sql
@@ -1,10 +1,12 @@
 SELECT 
 tl.id,
-density_id,
-class_yarn_id,
+-- density_id,
+-- class_yarn_id,
+yt.yarn_name AS type,
 density,
 length,
 length*density/1000000 AS weight
 FROM tape_length tl
 JOIN tape_density td ON tl.density_id = td.id
+JOIN yarn_type yt ON tl.class_yarn_id = yt.yarn_id
 ORDER BY class_yarn_id, density_id
